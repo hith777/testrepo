0ed866c87d6b63c23a4f27c455f54502ae69912e
hith777 <hithesh3232.5@gmail.com>
2020-08-11 17:32:30 +0530

    Add files via upload
    java code

---
+++ Arron.java
@@ -0,0 +1,15 @@
+class Arron 
+{
+	public static void main(String[] args) 
+	{
+		int n=new java.util.Scanner(System.in).nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(i+","+j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
